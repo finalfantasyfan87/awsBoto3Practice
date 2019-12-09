86d75e762584efccae5ba2c1b5e0d168b4404726
Jenni Hay <finalfantasyguru8705@gmail.com>
2019-12-08 23:53:34 -0600

    comments

--- s3/createS3bucket.py
+++ s3/createS3bucket.py
@@ -4,3 +4,5 @@ s3 = boto3.client('s3')
 
 s3.create_bucket(Bucket='jens-buckie')
 
+#log into AWS console and verify your new bucket was created in s3
+
--- s3/get_list_of_buckets.py
+++ s3/get_list_of_buckets.py
@@ -6,3 +6,11 @@ s3 = boto3.client('s3')
 my_buckies = s3.list_buckets()
 for bucket in my_buckies['Buckets']:
     print('Bucket', bucket)
+    
+#Prints the follow:
+# Bucket {'Name': 'aws-codestar-us-west-2-199186439076-cityofchicagoda-pipe', 'CreationDate': datetime.datetime(2019, 12, 9, 1, 35, 20, tzinfo=tzutc())}
+# Bucket {'Name': 'elasticbeanstalk-us-east-2-199186439076', 'CreationDate': datetime.datetime(2018, 6, 5, 18, 13, 35, tzinfo=tzutc())}
+# Bucket {'Name': 'elasticbeanstalk-us-west-1-199186439076', 'CreationDate': datetime.datetime(2019, 12, 9, 1, 34, 20, tzinfo=tzutc())}
+# Bucket {'Name': 'elasticbeanstalk-us-west-2-199186439076', 'CreationDate': datetime.datetime(2019, 5, 28, 3, 36, 21, tzinfo=tzutc())}
+# Bucket {'Name': 'jens-buckie', 'CreationDate': datetime.datetime(2019, 12, 9, 5, 1, 38, tzinfo=tzutc())}
+# Process finished with exit code 0
