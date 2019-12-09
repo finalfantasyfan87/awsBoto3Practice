b9657a7a52af85b12c41ec4c00c5c0d1b6b7ceb0
Jenni Hay <finalfantasyguru8705@gmail.com>
2019-12-09 01:14:21 -0600

    Create delete_a_s3bucket.py

---
+++ s3/delete_a_s3bucket.py
@@ -0,0 +1,9 @@
+import boto3
+import logging
+s3 = boto3.client('s3')
+""" :type : pyboto3.s3 """
+#prompt user for bucket name
+bucket_to_delete = input("Please enter the exact name of the bucket you want to delete: ")
+print(bucket_to_delete)
+# verify bucket is gone...if it isn't you probaly got an error or it might being the process of deleting
+s3.delete_bucket(Bucket=bucket_to_delete)
